297216c06d253ca157a4f3a9f17aa8082a6c4bcf
Roland09 <roland.cidar@gmail.com>
2020-12-12 09:11:59 +0100

    Specific drop targets for presets

--- Assets/Yapp/Editor/Modules/PrefabModuleEditor.cs
+++ Assets/Yapp/Editor/Modules/PrefabModuleEditor.cs
@@ -12,6 +12,40 @@ namespace Yapp
         PrefabPainter gizmo;
         #pragma warning restore 0414
 
+
+        struct PrefabPreset
+        {
+            public enum Type
+            {
+                Default, Object, Plant, Rock, House, Fence
+            }
+
+            public Type TypeId { get; set; }
+            public string Name { get; set; }
+            public bool RandomRotation { get; set; }
+        }
+
+        static PrefabPreset presetDefault = new PrefabPreset() { TypeId = PrefabPreset.Type.Default, Name = "", RandomRotation = false };
+        static PrefabPreset presetObject = new PrefabPreset() { TypeId = PrefabPreset.Type.Object, Name = "Object", RandomRotation = false };
+        static PrefabPreset presetPlant = new PrefabPreset() { TypeId = PrefabPreset.Type.Plant, Name = "Plant", RandomRotation = false };
+        static PrefabPreset presetRock = new PrefabPreset() { TypeId = PrefabPreset.Type.Rock, Name = "Rock", RandomRotation = true };
+        static PrefabPreset presetHouse = new PrefabPreset() { TypeId = PrefabPreset.Type.House, Name = "House", RandomRotation = false };
+        static PrefabPreset presetFence = new PrefabPreset() { TypeId = PrefabPreset.Type.Fence, Name = "Fence", RandomRotation = false };
+
+        // register the available prefab presets
+        List<PrefabPreset> prefabPresets = new List<PrefabPreset>()
+        {
+            presetPlant,
+            presetRock,
+            presetFence,
+            presetHouse,
+            presetObject,
+            // presetDefault, // default isn't registered, it's just used as a filler
+        };
+
+        // number of prefab preset drop targets per row in the preset grid
+        private int prefabPresetGridColumnCount = 4;
+
         public PrefabModuleEditor(PrefabPainterEditor editor)
         {
             this.editor = editor;
@@ -26,83 +60,96 @@ namespace Yapp
 
                 EditorGUILayout.LabelField("Prefabs", GUIStyles.BoxTitleStyle);
 
+                #region preset drop targets
                 GUILayout.BeginHorizontal();
                 {
-
                     GUILayout.BeginVertical();
                     {
                         // change background color in case there are no prefabs yet
                         if (gizmo.prefabSettingsList.Count == 0)
                         {
+                            EditorGUILayout.HelpBox("Drop prefabs on the prefab type boxes in order to use them.", MessageType.Info);
+
                             editor.SetErrorBackgroundColor();
                         }
 
-                        // drop area
-                        Rect prefabDropArea = GUILayoutUtility.GetRect(0.0f, 34.0f, GUIStyles.DropAreaStyle, GUILayout.ExpandWidth(true));
+                        int gridRows = Mathf.CeilToInt( (float) prefabPresets.Count / prefabPresetGridColumnCount);
 
-                        // drop area box with background color and info text
-                        GUI.color = GUIStyles.DropAreaBackgroundColor;
-                        GUI.Box(prefabDropArea, "Drop prefabs here in order to use them", GUIStyles.DropAreaStyle);
-                        GUI.color = GUIStyles.DefaultBackgroundColor;
+                        for (int row = 0; row < gridRows; row++)
+                        {
+                            GUILayout.BeginHorizontal();
+                            {
+                                for (int column = 0; column < prefabPresetGridColumnCount; column++)
+                                {
+                                    int index = column + row * prefabPresetGridColumnCount;
+
+                                    PrefabPreset preset = index < prefabPresets.Count ? preset = prefabPresets[index] : presetDefault;
+                                    string name = preset.Name;
+
+                                    // drop area
+                                    Rect prefabDropArea = GUILayoutUtility.GetRect(0.0f, 34.0f, GUIStyles.DropAreaStyle , GUILayout.ExpandWidth(true));
+
+                                    // drop area box with background color and info text
+                                    GUI.color = GUIStyles.DropAreaBackgroundColor;
+                                    GUI.Box(prefabDropArea, name, GUIStyles.DropAreaStyle);
+                                    GUI.color = GUIStyles.DefaultBackgroundColor;
+
+                                    Event evt = Event.current;
+                                    switch (evt.type)
+                                    {
+                                        case EventType.DragUpdated:
+                                        case EventType.DragPerform:
+
+                                            if (prefabDropArea.Contains(evt.mousePosition))
+                                            {
+
+                                                DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+
+                                                if (evt.type == EventType.DragPerform)
+                                                {
+                                                    DragAndDrop.AcceptDrag();
+
+                                                    // list of new prefabs that should be created via drag/drop
+                                                    // we can't do it in the drag/drop code itself, we'd get exceptions like
+                                                    //   ArgumentException: Getting control 12's position in a group with only 12 controls when doing dragPerform. Aborting
+                                                    // followed by
+                                                    //   Unexpected top level layout group! Missing GUILayout.EndScrollView/EndVertical/EndHorizontal? UnityEngine.GUIUtility:ProcessEvent(Int32, IntPtr)
+                                                    // they must be added when everything is done (currently at the end of this method)
+                                                    editor.newDraggedPrefabs = new List<PrefabSettings>();
+
+                                                    foreach (Object droppedObject in DragAndDrop.objectReferences)
+                                                    {
+
+                                                        // allow only prefabs
+                                                        if (PrefabUtility.GetPrefabAssetType(droppedObject) == PrefabAssetType.NotAPrefab)
+                                                        {
+                                                            Debug.Log("Not a prefab: " + droppedObject);
+                                                            continue;
+                                                        }
+
+                                                        // add the prefab to the list using the preset
+                                                        AddPrefab(droppedObject as GameObject, preset);
+
+                                                    }
+                                                }
+                                            }
+                                            break;
+                                    }
+                                }
+                            }
+                            GUILayout.EndHorizontal();
+                        }
 
                         editor.SetDefaultBackgroundColor();
 
-                        Event evt = Event.current;
-                        switch (evt.type)
-                        {
-                            case EventType.DragUpdated:
-                            case EventType.DragPerform:
-
-                                if (prefabDropArea.Contains(evt.mousePosition))
-                                {
-
-                                    DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
-
-                                    if (evt.type == EventType.DragPerform)
-                                    {
-                                        DragAndDrop.AcceptDrag();
-
-                                        // list of new prefabs that should be created via drag/drop
-                                        // we can't do it in the drag/drop code itself, we'd get exceptions like
-                                        //   ArgumentException: Getting control 12's position in a group with only 12 controls when doing dragPerform. Aborting
-                                        // followed by
-                                        //   Unexpected top level layout group! Missing GUILayout.EndScrollView/EndVertical/EndHorizontal? UnityEngine.GUIUtility:ProcessEvent(Int32, IntPtr)
-                                        // they must be added when everything is done (currently at the end of this method)
-                                        editor.newDraggedPrefabs = new List<PrefabSettings>();
-
-                                        foreach (Object droppedObject in DragAndDrop.objectReferences)
-                                        {
-
-                                            // allow only prefabs
-                                            if (PrefabUtility.GetPrefabAssetType(droppedObject) == PrefabAssetType.NotAPrefab)
-                                            {
-                                                Debug.Log("Not a prefab: " + droppedObject);
-                                                continue;
-                                            }
-
-                                            // new settings
-                                            PrefabSettings prefabSettings = new PrefabSettings();
-
-                                            // initialize with dropped prefab
-                                            prefabSettings.prefab = droppedObject as GameObject;
-
-                                            editor.newDraggedPrefabs.Add(prefabSettings);
-
-                                        }
-                                    }
-                                }
-                                break;
-                        }
-
                     }
-
-
                     GUILayout.EndVertical();
-
                 }
-
                 GUILayout.EndHorizontal();
 
+                #endregion preset drop targets
+
+                EditorGUILayout.Space();
 
                 for (int i = 0; i < gizmo.prefabSettingsList.Count; i++)
                 {
@@ -212,5 +259,25 @@ namespace Yapp
         {
         }
 
+        private void AddPrefab(GameObject prefab, PrefabPreset preset)
+        {
+            // new settings
+            PrefabSettings prefabSettings = new PrefabSettings();
+
+            // apply preset to prefab settings
+            // TODO: might be better in the constructor, be it might be necessary to do some aftermath in the editor, so leaving it at this
+            ApplyPreset(prefabSettings, preset);
+
+            // initialize with dropped prefab
+            prefabSettings.prefab = prefab;
+
+            editor.newDraggedPrefabs.Add(prefabSettings);
+
+        }
+
+        private void ApplyPreset(PrefabSettings prefabSettings, PrefabPreset preset)
+        {
+            prefabSettings.randomRotation = preset.RandomRotation;
+        }
     }
 }
