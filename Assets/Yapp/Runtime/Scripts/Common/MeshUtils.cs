32b018eff1121edab76ba7aee45d04d0974a3afe
Roland09 <roland.cidar@gmail.com>
2021-08-19 11:15:09 +0200

    consider submeshes (eg nm trees)

--- Assets/Yapp/Runtime/Scripts/Common/MeshUtils.cs
+++ Assets/Yapp/Runtime/Scripts/Common/MeshUtils.cs
@@ -9,39 +9,48 @@ namespace Rowlan.Yapp
 
         /// <summary>
         /// Render a game object using DrawMesh.
-        /// TODO: this doesn't consider submeshes (eg leaves on naturemanufacture trees)
         /// </summary>
         /// <param name="gameObject"></param>
         /// <param name="sourceLODLevel"></param>
         public static void RenderGameObject(GameObject gameObject, int sourceLODLevel)
         {
-            GameObject root = gameObject;
+            GameObject[] roots = new GameObject[] { gameObject };
 
             LODGroup lodGroup = gameObject.GetComponent<LODGroup>();
             if (lodGroup && lodGroup.lodCount > 0)
             {
-                root = lodGroup.GetLODs()[sourceLODLevel].renderers[0].gameObject;
+                // considering submeshes as well
+                roots = new GameObject[lodGroup.GetLODs()[sourceLODLevel].renderers.Length];
+
+                for( int i=0; i < roots.Length; i++)
+                {
+                    roots[i] = lodGroup.GetLODs()[sourceLODLevel].renderers[i].gameObject;
+                }
             }
 
-            MeshRenderer[] renderers = root.GetComponentsInChildren<MeshRenderer>();
-            for (int i = 0; i < renderers.Length; i++)
+            foreach (GameObject root in roots)
             {
-                MeshFilter meshFilter = renderers[i].gameObject.GetComponent<MeshFilter>();
-                if (meshFilter)
+                MeshRenderer[] renderers = root.GetComponentsInChildren<MeshRenderer>();
+                for (int i = 0; i < renderers.Length; i++)
                 {
-                    Matrix4x4 matrix = Matrix4x4.TRS(
-                        renderers[i].transform.position,
-                        renderers[i].transform.rotation,
-                        renderers[i].transform.lossyScale);
-
-                    Mesh mesh = meshFilter.sharedMesh;
-
-                    for (int j = 0; j < renderers[i].sharedMaterials.Length; j++)
+                    MeshFilter meshFilter = renderers[i].gameObject.GetComponent<MeshFilter>();
+                    if (meshFilter)
                     {
-                        Material material = renderers[i].sharedMaterials[j];
-                        material.SetPass(0);
+                        Matrix4x4 matrix = Matrix4x4.TRS(
+                            renderers[i].transform.position,
+                            renderers[i].transform.rotation,
+                            renderers[i].transform.lossyScale);
 
-                        Graphics.DrawMesh(mesh, matrix, material, 0);
+                        Mesh mesh = meshFilter.sharedMesh;
+
+                        for (int j = 0; j < renderers[i].sharedMaterials.Length; j++)
+                        {
+                            Material material = renderers[i].sharedMaterials[j];
+                            material.SetPass(0);
+
+                            // TODO: use this in later Unity versions: https://docs.unity3d.com/2021.2/Documentation/ScriptReference/Graphics.RenderMesh.html
+                            Graphics.DrawMesh(mesh, matrix, material, 0);
+                        }
                     }
                 }
             }
